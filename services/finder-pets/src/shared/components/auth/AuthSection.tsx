52d7aebbd236d4a0cca690cf42d8f9543ac89247
DataCodeLiteracy <dataliteracy@icloud.com>
2024-04-29 02:28:37 +0900

    [ 🚚 | #1 | chore ] no-unused-vars 규칙 warn으로 제대로 적용이 안되서 eslint 규칙 수정
    2024.04.29

---
+++ services/finder-pets/src/shared/components/auth/AuthSection.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+type Props = {
+  header: ReactNode;
+  main: ReactNode;
+  footer: ReactNode;
+};
+
+const AuthSection = ({ header, main, footer }: Props) => {
+  return <section></section>;
+};
+
+export default AuthSection;
