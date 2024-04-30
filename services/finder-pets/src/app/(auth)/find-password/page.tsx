dd989e7daff3fc9d449d3427451b5649144458c6
DataCodeLiteracy <dataliteracy@icloud.com>
2024-05-01 05:23:22 +0900

    [ ✨ | #2 | feature ] 비밀번호 찾기 페이지 & 결과 페이지 구현
    2024.05.01

--- services/finder-pets/src/app/(auth)/find-password/page.tsx
+++ services/finder-pets/src/app/(auth)/find-password/page.tsx
@@ -1,5 +1,16 @@
+import FindPasswordButtons from "@/components/auth/FindPasswordButtons";
+import FindPasswordMain from "@/components/auth/FindPasswordMain";
+import AuthHeader from "@/shared/components/auth/AuthHeader";
+import AuthSection from "@/shared/components/auth/AuthSection";
+
 const FindPassword = () => {
-  return <div>비밀번호 찾기</div>;
+  return (
+    <AuthSection
+      header={<AuthHeader title="비밀번호 찾기" />}
+      main={<FindPasswordMain />}
+      footer={<FindPasswordButtons />}
+    />
+  );
 };
 
 export default FindPassword;
