2957e18a66626791c4b1b3181db84d0e1c0b6463
DataCodeLiteracy <dataliteracy@icloud.com>
2024-05-04 01:04:19 +0900

    [ ✨ | #2 | feature ] 실종, 목격 페이지 리스트 추가해서 구현
    2024.05.04

--- services/finder-pets/src/app/(finder)/finder/lost/page.tsx
+++ services/finder-pets/src/app/(finder)/finder/lost/page.tsx
@@ -1,5 +1,19 @@
+import ListBox from "@/components/list/ListBox";
+import Spacing from "@/shared/components/Spacing";
+import FilterMenuBar from "@/shared/components/filter/FilterMenuBar";
+
 const Lost = () => {
-  return <div>실종</div>;
+  return (
+    <section>
+      <Spacing height="20px" />
+      <FilterMenuBar />
+      <Spacing height="12px" />
+      <ListBox />
+      <ListBox />
+      <ListBox />
+      <ListBox />
+    </section>
+  );
 };
 
 export default Lost;
--- services/finder-pets/src/app/(finder)/finder/sighted/page.tsx
+++ services/finder-pets/src/app/(finder)/finder/sighted/page.tsx
@@ -1,5 +1,20 @@
+import ListBox from "@/components/list/ListBox";
+import Spacing from "@/shared/components/Spacing";
+import FilterMenuBar from "@/shared/components/filter/FilterMenuBar";
+
 const Sighted = () => {
-  return <div>목격</div>;
+  return (
+    <section>
+      <Spacing height="20px" />
+      <FilterMenuBar />
+      <Spacing height="12px" />
+      <ListBox />
+      <ListBox />
+      <ListBox />
+      <ListBox />
+      <ListBox />
+    </section>
+  );
 };
 
 export default Sighted;
