0e3ea218efa86e2c8f1df991512e5e6d2c0c6878
DataCodeLiteracy <dataliteracy@icloud.com>
2024-05-06 04:54:16 +0900

    [ ✨ | #2 | feature ] ProfileButtons 추가해서 프로필 페이지 구현완료
    2024.05.06

--- services/finder-pets/src/app/(auth)/my-menu/profile/page.tsx
+++ services/finder-pets/src/app/(auth)/my-menu/profile/page.tsx
@@ -1,8 +1,9 @@
+import ProfileButtons from "@/components/profile/ProfileButtons";
 import ProfileMain from "@/components/profile/ProfileMain";
 import EditorSection from "@/shared/components/editor/EditorSection";
 
 const Profile = () => {
-  return <EditorSection title="프로필" main={<ProfileMain />} footer={<></>} />;
+  return <EditorSection title="프로필" main={<ProfileMain />} footer={<ProfileButtons />} />;
 };
 
 export default Profile;
