7f437ca5a3e5f0b1539862fe15fbf353361ea907
DataCodeLiteracy <dataliteracy@icloud.com>
2024-05-09 03:44:24 +0900

    [ ✨ | #3 | feature ] 로그인 Mock API 구현 및 테스트
    2024.05.09

---
+++ services/finder-pets/src/app/api/mocks/postLogInUser.ts
@@ -0,0 +1,19 @@
+import { User } from "@/models/user";
+import { HttpHandler, HttpResponse, http } from "msw";
+
+const users: User[] = JSON.parse(localStorage.getItem("users") || "[]");
+
+export const postLogInUser: HttpHandler = http.post("/api/login", async ({ request }) => {
+  const { email, password } = (await request.json()) as User;
+
+  const user = users.find((u) => u.email === email && u.password === password);
+
+  if (user) {
+    return HttpResponse.json(user, { status: 200 });
+  } else {
+    return HttpResponse.json(
+      { message: "이메일 또는 비밀번호가 잘못되었습니다." },
+      { status: 401 },
+    );
+  }
+});
--- services/finder-pets/src/shared/mocks/handler.ts
+++ services/finder-pets/src/shared/mocks/handler.ts
@@ -1,5 +1,6 @@
 import { getUser } from "@/app/api/mocks/getUser";
+import { postLogInUser } from "@/app/api/mocks/postLogInUser";
 import { postRegisterUser } from "@/app/api/mocks/postRegistUser";
 import { HttpHandler } from "msw";
 
-export const handlers: HttpHandler[] = [getUser, postRegisterUser];
+export const handlers: HttpHandler[] = [getUser, postRegisterUser, postLogInUser];
