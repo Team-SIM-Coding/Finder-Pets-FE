033b2979e13a3850b27e647120e09f16cf9b9bef
DataCodeLiteracy <dataliteracy@icloud.com>
2024-05-10 04:18:27 +0900

    [ 🔧 | #2 | fix ] 반려동물 등록 Form 수정
    2024.05.10

--- services/finder-pets/src/components/submit/my-pet/MyPetRegisterForm.tsx
+++ services/finder-pets/src/components/submit/my-pet/MyPetRegisterForm.tsx
@@ -1,24 +1,14 @@
 "use client";
 
 import EditorSection from "@/shared/components/editor/EditorSection";
-import MyPetRegisterMain from "./MyPetRegisterMain";
-import { useEffect, useState } from "react";
-import { LostPets } from "@/shared/types/pet";
 import MyPetRegisterButton from "./MyPetRegisterButton";
+import MyPetRegisterMain from "./MyPetRegisterMain";
 
 const MyPetRegisterForm = () => {
-  const [item, setItem] = useState<LostPets>({} as LostPets);
-
-  useEffect(() => {
-    fetch("/mocks/lost_mock_data.json")
-      .then((res) => res.json())
-      .then((data) => setItem(data[0]));
-  }, []);
-
   return (
     <EditorSection
       title="반려동물 등록"
-      main={<MyPetRegisterMain pet_info={item} />}
+      main={<MyPetRegisterMain />}
       footer={<MyPetRegisterButton />}
     />
   );
