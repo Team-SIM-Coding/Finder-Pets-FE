93d0ec996f10228f9644895f82465e8e7359f81f
DataCodeLiteracy <dataliteracy@icloud.com>
2024-05-10 03:08:59 +0900

    [ ✨ | #3 | feature ] 중복된 이메일, 중복된 반려동물 이름 검사하는 로직 추가
    2024.05.10

--- services/finder-pets/src/app/api/mocks/postMyPet.ts
+++ services/finder-pets/src/app/api/mocks/postMyPet.ts
@@ -6,6 +6,15 @@ const pets: Pet[] = JSON.parse(localStorage.getItem("my-pets") || "[]");
 export const postMyPet: HttpHandler = http.post("/api/my-pet/register", async ({ request }) => {
   const newMyPet = (await request.json()) as Pet;
 
+  const isDuplicate = pets.some((pet) => pet.name === newMyPet.name);
+
+  if (isDuplicate) {
+    return HttpResponse.json(
+      { message: `이미 등록된 이름 '${newMyPet.name}'이 있습니다.` },
+      { status: 409 },
+    );
+  }
+
   pets.push(newMyPet);
 
   localStorage.setItem("pets", JSON.stringify(pets));
--- services/finder-pets/src/app/api/mocks/postRegistUser.ts
+++ services/finder-pets/src/app/api/mocks/postRegistUser.ts
@@ -6,6 +6,12 @@ const users: User[] = JSON.parse(localStorage.getItem("users") || "[]");
 export const postRegisterUser: HttpHandler = http.post("/api/register", async ({ request }) => {
   const newUser = (await request.json()) as User;
 
+  const isDuplicate = users.some((user) => user.email === newUser.email);
+
+  if (isDuplicate) {
+    return HttpResponse.json({ message: "이미 등록된 이메일이 있습니다." }, { status: 409 });
+  }
+
   users.push(newUser);
 
   localStorage.setItem("users", JSON.stringify(users));
