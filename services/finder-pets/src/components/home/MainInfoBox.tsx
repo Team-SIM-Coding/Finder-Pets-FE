d572e17d327d8e493efd383c4cf278e8b2370ae0
DataCodeLiteracy <dataliteracy@icloud.com>
2024-05-14 13:52:39 +0900

    [ ✨ | #2 | feature ] home 화면 더보기 버튼 Link 추가
    2024.05.14

--- services/finder-pets/src/components/home/MainInfoBox.tsx
+++ services/finder-pets/src/components/home/MainInfoBox.tsx
@@ -8,6 +8,7 @@ import { Flex } from "@design-system/react-components-layout";
 import MainSwiperBox from "@/shared/components/swiper/MainSwiperBox";
 import { useEffect, useState } from "react";
 import { LostPets } from "@/shared/types/pet";
+import Link from "next/link";
 
 interface Props {
   title: string;
@@ -27,7 +28,12 @@ const MainInfoBox = ({ title }: Props) => {
       <Flex justify="space-between" className={s.titleWrap}>
         <h2 className={cs.subHeader}>{title}</h2>
         <Flex justify="center" align="center" className={s.textAndIconWrap}>
-          <span>더보기</span>
+          <Link
+            href={title === "기다림의 끝에서" ? "/finder/lost" : "/finder/sighted"}
+            className={s.linkStyle}
+          >
+            <span>더보기</span>
+          </Link>
           <IoIosArrowForward className={s.iconStyle} />
         </Flex>
       </Flex>
